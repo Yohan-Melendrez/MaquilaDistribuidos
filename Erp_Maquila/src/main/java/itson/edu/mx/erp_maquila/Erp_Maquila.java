ea627deb8ace7308a5ee16e69f0bde878fcf97bc
Gabriel <140923456+Yohan-Melendrez@users.noreply.github.com>
2025-04-16 21:16:38 -0700

    proyectos iniciales

---
+++ Erp_Maquila/src/main/java/itson/edu/mx/erp_maquila/Erp_Maquila.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.edu.mx.erp_maquila;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Erp_Maquila {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ QA_Maquila/src/main/java/itson/edu/mx/qa_maquila/QA_Maquila.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.edu.mx.qa_maquila;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class QA_Maquila {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
