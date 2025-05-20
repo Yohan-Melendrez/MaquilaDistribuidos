0e32f24ea7da53fd6b3f1a5f07f7ca6db48559ae
PabloGalan13 <pablo.galan244752@potros.itson.edu.mx>
2025-05-20 05:03:14 -0700

    se prendio el cerro
    xd

--- servicio-qa/src/main/java/service/qa/BasicApplication.java
+++ servicio-qa/src/main/java/service/qa/BasicApplication.java
@@ -8,8 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableRabbit
 public class BasicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BasicApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.setProperty("javax.net.ssl.trustStore", "C:\\Users\\USER\\Documents\\ITSON\\Trabajos ITSON\\Sexto Semestre\\Sistemas Distribuidos\\Proyecto\\MaquilaDistribuidos\\truststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
+        System.setProperty("javax.net.ssl.trustStoreType","JKS");
+        SpringApplication.run(BasicApplication.class, args);
+        
+    }
 }
